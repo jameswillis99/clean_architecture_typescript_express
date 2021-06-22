eb1b9d8014782532f33293e0fa808f158dc705b4
James W <james.willis@raytheon.co.uk>
2021-06-22 19:07:20 +0100

    fixed tests

--- src/infrastructure/repo/DogsRepo.ts
+++ src/infrastructure/repo/DogsRepo.ts
@@ -7,17 +7,15 @@ import Dog from './sqlite/entity/DogEntity';
  * Facade Design Pattern
  */
 export default class DogsRepoSample implements DogsRepo {
-  private connection: Connection;
-
-  constructor() {
-    this.connection = getConnection();
-  }
+  private connection!: Connection;
 
   async getDogs() {
+    this.connection = getConnection();
     return this.connection.getRepository(Dog).createQueryBuilder('dog').getMany();
   }
 
   async postDogs(dogs: DogsModel[]) {
+    this.connection = getConnection();
     await this.connection.createQueryBuilder().insert().into(Dog).values(dogs)
       .execute();
   }
